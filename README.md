ffa9a8994c2ff9b4c77e0a4c24f9d25b151b69fa
dmitry zhigulin <jigulin.d@gmail.com>
2022-06-30 21:31:20 +0400

    Exercises after 1.1.6

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # SICP exercises
+
+### Requirements
+
+ *  Racket
+ *  Rackunit (`raco pkg install rackunit`)
+ *  [SICP Support for DrRacket](https://docs.racket-lang.org/sicp-manual/index.html) (`raco pkg install sicp`)
+ *  [Racket-review](https://github.com/Bogdanp/racket-review) surface-level linter (`raco pkg install review`)
+ *  Make
+
+This repository based on [hexlet-boilerplates/sicp-racket](https://github.com/hexlet-boilerplates/sicp-racket)
---
+++ sicp/chapter1/03.rkt
@@ -0,0 +1,28 @@
+#lang sicp
+
+(#%require rackunit)
+
+(define (square a)
+  (* a a))
+
+(define (sum-of-squares a b)
+  (+ (square b) (square a)))
+
+(define (>= a b)
+  (not (< a b)))
+
+(define (square-sum-of-two-largest a b c)
+  (cond ((and (>= a c)
+              (>= b c))
+          (sum-of-squares a b))
+        ((and (>= a b)
+              (>= c b))
+          (sum-of-squares a c))
+        (else
+          (sum-of-squares b c))))
+
+(check-equal? (square-sum-of-two-largest 1 2 3) 13)
+(check-equal? (square-sum-of-two-largest 4 2 3) 25)
+(check-equal? (square-sum-of-two-largest 0 0 0) 0)
+(check-equal? (square-sum-of-two-largest 1 0 1) 2)
+(check-equal? (square-sum-of-two-largest -5 3 2) 13)
